0f0766fd5fe690eead0fe03ee7c2e344227d27e5
Michael Cronin <michael.n.cronin@duke.edu>
2023-11-20 19:18:26 +0100

    some auth filed add

--- beli/auth.py
+++ beli/auth.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from . import db, bcrypt
-
 from . import model
+import flask_login
 
 bp = Blueprint("auth", __name__)
 
@@ -18,14 +18,46 @@ def signup_post():
     password = request.form.get("password")
     # Check that passwords are equal
     if password != request.form.get("password_repeat"):
+        flash("Sorry, passwords are different")
         return redirect(url_for("auth.signup"))
     # Check if the email is already at the database
     query = db.select(model.User).where(model.User.email == email)
     user = db.session.execute(query).scalar_one_or_none()
     if user:
+        flash("Sorry, the email you provided is already registered")
         return redirect(url_for("auth.signup"))
     password_hash = bcrypt.generate_password_hash(password).decode("utf-8")
     new_user = model.User(email=email, name=username, password=password_hash)
     db.session.add(new_user)
     db.session.commit()
+    flash("You've successfully signed up!")
     return redirect(url_for("main.index"))
+
+
+@bp.route("/login")
+def login():
+    return render_template("auth/login.html")
+
+
+@bp.route("/login", methods=["POST"])
+def login_post():
+    email = request.form.get("email")
+    password = request.form.get("password")
+    # Check if the email is already at the database
+    query = db.select(model.User).where(model.User.email == email)
+    user = db.session.execute(query).scalar_one_or_none()
+    # if the user exists, redirect to main page
+    if user and bcrypt.check_password_hash(user.password, password):
+        flask_login.login_user(user)
+        return redirect(url_for("main.index"))
+    # wrong  email and password
+    else:
+        flash("Sorry, the email and password you provided do not exist.")
+        return redirect(url_for("auth.login"))
+    
+# log out
+@bp.route("/logout")
+@flask_login.login_required
+def log_out():
+    flask_login.logout_user()
+    return redirect(url_for("auth.login"))
