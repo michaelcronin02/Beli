ca0c3b40ab5eec2079c02f8b895511a479ffacf1
Michael Cronin <michael.n.cronin@duke.edu>
2023-11-20 16:45:56 +0100

    idfk

--- New Material/Beli-App/__init__.py
+++ New Material/Beli-App/__init__.py
@@ -1,8 +1,15 @@
 from flask import Flask
 
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def create_app(test_config=None):
     app = Flask(__name__)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///microblog.db"
+    
+    db.init_app(app)
 
     # A secret for signing session cookies
     app.config["SECRET_KEY"] = "93220d9b340cf9a6c39bac99cce7daf220167498f91fa"
