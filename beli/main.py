cd94906b42054a77fde75dad14e271d7abedf717
Michael Cronin <michael.n.cronin@duke.edu>
2023-11-23 01:41:33 +0100

    Recipe posts working now

--- beli/main.py
+++ beli/main.py
@@ -14,23 +14,91 @@ bp = Blueprint("main", __name__)
 @bp.route("/")
 @flask_login.login_required
 def index():
-    user = model.User(email="mary@example.com", name="mary", )
-    posts = [
-        model.Recipe(
-            user=user,
-            title="Beans",
-            description="many beans",
-            user_id=user,
-            servings = 4,
-            cook_time = 15,
-        ),
-        model.Recipe(
-            user=user,
-            title="Eggs",
-            description="many eggs",
-            user_id=user,
-            servings = 1,
-            cook_time = 30,
-        ),
-    ]
+    followers = db.aliased(model.User)
+    query = (
+    db.select(model.Recipe)
+    .join(model.User)
+    .join(followers, model.User.followers)
+    .where(followers.id == flask_login.current_user.id)
+    .limit(10))
+    posts = db.session.execute(query).scalars().all()
     return render_template("main/index.html", posts=posts)
+
+
+@bp.route("/user/<int:user_id>")
+@flask_login.login_required
+def userProfile(user_id):
+    user = db.session.get(model.User, user_id)
+    if not user:
+        abort(404, "User id {} doesn't exist.".format(user_id))
+    query = db.select(model.Recipe).where((model.Recipe.user_id == user_id))
+    posts = db.session.execute(query).scalars().all()
+
+    if user == current_user:
+        follow_button = None
+    if flask_login.current_user not in user.followers:
+        follow_button = "follow"
+    if flask_login.current_user in user.followers:
+        follow_button = "unfollow"
+
+    return render_template("main/profile.html", posts=posts, user=user, follow_button=follow_button)
+
+
+@bp.route("/new_post", methods=['POST'])
+@flask_login.login_required
+def new_post():
+    recipe_title = request.form.get("title")
+    recipe_description = request.form.get("description")
+    recipe_servings = request.form.get("servings")
+    recipe_cook_time = request.form.get("cook_time")
+    message = model.Recipe(user=flask_login.current_user,
+                         title = recipe_title,
+                         description = recipe_description,
+                         servings = recipe_servings,
+                         cook_time = recipe_cook_time,
+                        )
+
+    db.session.add(message)
+    db.session.commit()
+    return redirect(url_for("main.post", message_id=message.id))
+
+@bp.route("/post/<int:message_id>")
+@flask_login.login_required
+def post(message_id):
+    message = db.session.get(model.Recipe, message_id)
+    if not message:
+        abort(404, "Post id {} doesn't exist.".format(message_id))
+    return render_template("main/post.html", post=message)
+
+@bp.route("/follow/<int:user_id>", methods=["POST"])
+@flask_login.login_required
+def follow(user_id):
+    user = db.session.get(model.User, user_id)
+    query = db.select(model.Recipe).where((model.Recipe.user_id == user_id))
+    posts = db.session.execute(query).scalars().all()
+    if not user:
+        abort(404, "User id {} doesn't exist.".format(user_id))
+    if user == current_user:
+        abort(403, "User id {} is your user.".format(user_id))
+    if flask_login.current_user in user.followers:
+        abort(403, "User id {} is already followed.".format(user_id))
+    user.followers.append(flask_login.current_user)
+    db.session.commit()
+    return redirect(url_for("main.userProfile", user_id=user_id))
+
+@bp.route("/unfollow/<int:user_id>", methods=["POST"])
+@flask_login.login_required
+def unfollow(user_id):
+    user = db.session.get(model.User, user_id)
+    query = db.select(model.Recipe).where((model.Recipe.user_id == user_id))
+    posts = db.session.execute(query).scalars().all()
+    if not user:
+        abort(404, "User id {} doesn't exist.".format(user_id))
+    if user == current_user:
+        abort(403, "User id {} is your user.".format(user_id))
+    if flask_login.current_user not in user.followers:
+        abort(403, "User id {} is not already followed.".format(user_id))
+    user.followers.remove(flask_login.current_user)
+    db.session.commit()
+    return redirect(url_for("main.userProfile", user_id=user_id))
+# return render_template("main/profile.html", user=user, posts=posts, follow_button=follow_button)
--- beli/model.py
+++ beli/model.py
@@ -15,7 +15,7 @@ class User(flask_login.UserMixin, db.Model):
     name = db.Column(db.String(64), nullable=False)
     password = db.Column(db.String(100), nullable=False)
     recipes = db.relationship('Recipe', back_populates='user')
-    ratings = db.relationship('Rating', back_populates='user')
+    #ratings = db.relationship('Rating', back_populates='user')
     following = db.relationship(
         "User",
         secondary=FollowingAssociation.__table__,
@@ -33,33 +33,33 @@ class User(flask_login.UserMixin, db.Model):
 
 class Recipe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(512), nullable=False)
+    title = db.Column(db.Text(512), nullable=False)
     description = db.Column(db.Text, nullable=False)
     servings = db.Column(db.Integer, nullable=False)
     cook_time = db.Column(db.Integer, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     user = db.relationship('User', back_populates='recipes')
-    quantified_ingredients = db.relationship('QuantifiedIngredient', back_populates='recipe')
-    ratings = db.relationship('Rating', back_populates='recipe')
+    #quantified_ingredients = db.relationship('QuantifiedIngredient', back_populates='recipe')
+    #ratings = db.relationship('Rating', back_populates='recipe')
 
-class Ingredient(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(512), nullable=False)
-    quantified_ingredients = db.relationship('QuantifiedIngredient', back_populates='ingredient')
+#class Ingredient(db.Model):
+#    id = db.Column(db.Integer, primary_key=True)
+#    name = db.Column(db.String(512), nullable=False)
+#    quantified_ingredients = db.relationship('QuantifiedIngredient', back_populates='ingredient')
 
-class QuantifiedIngredient(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    amount = db.Column(db.Float, nullable=False)
-    unit = db.Column(db.String(64), nullable=False)
-    ingredient_id = db.Column(db.Integer, db.ForeignKey('ingredient.id'), nullable=False)
-    ingredient = db.relationship('Ingredient', back_populates='quantified_ingredients')
-    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
-    recipe = db.relationship('Recipe', back_populates='quantified_ingredients')
+#class QuantifiedIngredient(db.Model):
+#    id = db.Column(db.Integer, primary_key=True)
+#    amount = db.Column(db.Float, nullable=False)
+#    unit = db.Column(db.String(64), nullable=False)
+#    ingredient_id = db.Column(db.Integer, db.ForeignKey('ingredient.id'), nullable=False)
+#    ingredient = db.relationship('Ingredient', back_populates='quantified_ingredients')
+#    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
+#    recipe = db.relationship('Recipe', back_populates='quantified_ingredients')
 
-class Rating(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    value = db.Column(db.Integer, nullable=False) # 0 is a like, 1 is a dislike
-    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
-    recipe = db.relationship('Recipe', back_populates='ratings')
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    user = db.relationship('User', back_populates='ratings')
+#class Rating(db.Model):
+#    id = db.Column(db.Integer, primary_key=True)
+#    value = db.Column(db.Integer, nullable=False) # 0 is a like, 1 is a dislike
+#    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
+#    recipe = db.relationship('Recipe', back_populates='ratings')
+#    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+#    user = db.relationship('User', back_populates='ratings')
